2450364f2a0be1cc1378c9aeec1550dc9cfaa223
hmttai <josephho382@gmail.com>
2023-05-18 10:51:44 +0700

    import library for index.js back-end 2

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 import express from "express";
 import cors from "cors";
+
+import SortMiddleware from "./app/middlewares/SortMiddleware";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import route from "./routes/index.js";
+import connect from "./config/db/index.js";
