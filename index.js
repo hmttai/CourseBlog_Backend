618abf6b86cf93abe00990ff6822820c72155551
hmttai <josephho382@gmail.com>
2023-05-18 10:53:34 +0700

    connect to DB

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const express = require("express");
-const SortMiddleware = require("./app/middlewares/SortMiddleware");
+// const express = require("express");
+// const SortMiddleware = require("./app/middlewares/SortMiddleware");
 
-const route = require("./routes");
-const db = require("./config/db");
+// const route = require("./routes");
+// const db = require("./config/db");
 
 import express from "express";
 import cors from "cors";
-
-import SortMiddleware from "./app/middlewares/SortMiddleware";
+//import SortMiddleware from "./app/middlewares/SortMiddleware";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import route from "./routes/index.js";
 import connect from "./config/db/index.js";
+
+//Connect to Db
+connect();
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
