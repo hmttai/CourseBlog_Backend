9e823647d1a833330e87efe8da999a06a6094800
hmttai <josephho382@gmail.com>
2023-05-19 09:41:54 +0700

    route: news.js

---
+++ src/routes/news.js
@@ -0,0 +1,10 @@
+// const express = require("express");
+import express from "express";
+const router = express.Router();
+
+import newsController from "../app/controllers/NewsController.js";
+
+router.get("/:slug", newsController.show);
+router.get("/", newsController.index);
+
+export default router;
