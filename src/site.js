0f6b4859170911cbffec61141cfe2da33b6a50fa
hmttai <josephho382@gmail.com>
2023-05-19 08:11:22 +0700

    routes: site.js

---
+++ src/site.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+
+import siteController from "../app/controllers/SiteController.js";
+
+router.get("/search", siteController.search);
+router.get("/", siteController.index);
+
+export default router;
